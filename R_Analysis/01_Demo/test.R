4191c20651c0744aa3f1df307573d4910b542d04
Tom Greff <tomjj.greff@gmail.com>
2020-08-27 13:47:12 -0400

    upload files

--- R_Analysis/01_Demo/test.R
+++ R_Analysis/01_Demo/test.R
@@ -18,3 +18,18 @@ all.equal(demo_table3, wide_table)
 demo_table3 <- demo_table3[,order(colnames(demo_table3))]
 wide_table <- wide_table[,order(colnames(wide_table))]
 all.equal(demo_table3, wide_table)
+plt <- ggplot(mpg,aes(x=class)) #import dataset into ggplot2
+plt + geom_bar() #plot a bar plot
+mpg_summary <- mpg %>% group_by(manufacturer) %>% summarize(Vehicle_Count=n()) #create summary table
+plt <- ggplot(mpg_summary,aes(x=manufacturer,y=Vehicle_Count)) #import dataset into ggplot2
+plt + geom_col() #plot a bar plot
+plt + geom_col() + xlab("Manufacturing Company") + ylab("Number of Vehicles in Dataset") + theme(axis.text.x=element_text(angle=45,hjust=1)) #plot bar plot with labels + rotate the x-axis label 45 degrees
+mpg_summary <- subset(mpg,manufacturer=="toyota") %>% group_by(cyl) %>% summarize(Mean_Hwy=mean(hwy)) #create summary table
+plt <- ggplot(mpg_summary,aes(x=cyl,y=Mean_Hwy)) #import dataset into ggplot2
+plt + geom_line() + scale_x_discrete(limits=c(4,6,8)) + scale_y_continuous(breaks = c(15:30)) #add line plot with labels
+plt <- ggplot(mpg,aes(x=displ,y=cty)) #import dataset into ggplot2
+plt + geom_point() + xlab("Engine Size (L)") + ylab("City Fuel-Efficiency (MPG)") #add scatter plot with labels
+plt <- ggplot(mpg,aes(x=displ,y=cty,color=class)) #import dataset into ggplot2
+plt + geom_point() + labs(x="Engine Size (L)", y="City Fuel-Efficiency (MPG)", color="Vehicle Class") #add scatter plot with labels
+plt <- ggplot(mpg,aes(x=displ,y=cty,color=class,shape=drv)) #import dataset into ggplot2
+plt + geom_point() + labs(x="Engine Size (L)", y="City Fuel-Efficiency (MPG)", color="Vehicle Class",shape="Type of Drive") #add scatter plot with multiple
