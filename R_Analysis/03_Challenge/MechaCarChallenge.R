5021e62a88a04d00627ae9db41a1a37b8558edb6
Tom Greff <tomjj.greff@gmail.com>
2020-08-29 18:09:51 -0400

    upload files

--- R_Analysis/03_Challenge/MechaCarChallenge.R
+++ R_Analysis/03_Challenge/MechaCarChallenge.R
@@ -1,2 +1,8 @@
 # 1. MPG Regression 
-mpg_table <- read.csv(file='Data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+mpg_table <- read.csv(file='Data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) # read and store the data
+summary(lm(mpg ~ `vehicle length` + `vehicle weight`+ `spoiler angle` + 
+     `ground clearance` + AWD, data=mpg_table)) #generate multiple linear regression model summary
+
+# 2. Suspension Coil Summary
+coil_table <- read.csv(file='Data/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) # read and store the data
+coil_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), Std_dev_PSI=sqrt(var(PSI))) #create a summary static table for each lot
